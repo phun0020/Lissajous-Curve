49a8f31d0d615883f34b0e9940f29edb470a95fa
Tam Phung <tam.ngocphung@gmail.com>
2019-04-05 20:19:26 -0400

    done with circles

--- Lissajous Curve/sketch.js
+++ Lissajous Curve/sketch.js
@@ -1,16 +1,31 @@
 let diameter, cols, rows;
 let horzCircles = [];
 let vertCircles = [];
-let curves = [];
+let curves;
 
 const space = 50;
 const speed = 0.005;
+
+function make2DArray(rows, cols) {
+    var arr = new Array(rows); //like arr[]; but with number of columns hardcoded
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(cols);
+    }
+    return arr;
+}
 
 function setup() {
     createCanvas(950, 950);
     cols = floor(width / 100);
     rows = floor(height / 100);
     diameter = cols * 10;
+    curves = make2DArray(rows, cols);
+    
+    for (let j = 0; j < rows; j++) {
+        for (let i = 0; i < cols; i++) {
+            curves[j][i] = new Curve();
+        }
+    }
 
     for (let i = 1; i <= cols; i++) {
         horzCircles = [
@@ -28,16 +43,27 @@ function setup() {
 function draw() {
     background(0);
     stroke(255);
+
     horzCircles.map(c => c.draw());
     vertCircles.map(c => c.draw());
-    
+
     for(let i = 0; i < cols; i++) {
+        // point x
         let x = horzCircles[i].point.x;
-        strokeWeight(8);
+        
         for(let j = 0; j < rows; j++) {
+            // point y
             let y = vertCircles[j].point.y;
-            point(x, y);
+            
+            curves[j][i].setX(x);
+            curves[j][i].setY(y);
         }
-        strokeWeight(1);
+    }
+
+    for (let j = 0; j < rows; j++) {
+        for (let i = 0; i < cols; i++) {
+            curves[j][i].addPoint();
+            curves[j][i].show();
+        }
     }
 }
